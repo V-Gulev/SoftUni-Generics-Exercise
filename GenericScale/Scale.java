b8affdf6aa9f6036db8c396140f07f1ee8370694
V-Gulev <velizargulev@abv.bg>
2025-02-04 21:10:23 +0200

    Generic Scale

---
+++ GenericScale/Scale.java
@@ -0,0 +1,25 @@
+package GenericScale;
+
+import java.util.Comparator;
+
+public class Scale<T extends Comparable<T>> {
+    private T left;
+    private T right;
+
+    public Scale(T left, T right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public T getHeavier(){
+        int result = left.compareTo(right);
+        if (result == 0) {
+            return null;
+        } else if (result >= 1) {
+            return left;
+        } else {
+            return right;
+        }
+    }
+
+}
